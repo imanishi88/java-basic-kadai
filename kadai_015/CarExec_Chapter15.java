b98a29241fcb08588b8474af7446bd39467627e5
imanishi88 <imanishi.0810@gmail.com>
2025-02-21 10:12:21 +0900

    課題015提出

---
+++ kadai_015/CarExec_Chapter15.java
@@ -0,0 +1,32 @@
+//車クラスを実行
+
+package kadai_15;
+
+
+//インスタンス化
+public class CarExec_Chapter15 {
+	public static void main(String[] args) {
+		
+		Car_Chapter15 car = new Car_Chapter15( 1, 10);
+		
+		
+		car.gearChange(3);
+		
+		car.run();
+		
+	}
+}
+
+//車クラスのgearChangeメソッドを実行する
+
+
+
+
+
+
+
+
+
+
+
+//車クラスのrunメソッドを実行する
